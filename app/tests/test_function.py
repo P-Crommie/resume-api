6e88748356574e7d1fea6c07b74fd975771b9e5e
Emmanuel Cromwell <immanuelcromwell@ymail.com>
2024-06-26 14:35:35 +0000

    UPDATE: test mock region fix#8

--- app/tests/test_function.py
+++ app/tests/test_function.py
@@ -15,8 +15,17 @@ def mock_environment():
         yield
 
 
+@pytest.fixture
+def mock_boto3_session():
+    with patch('boto3.Session') as MockSession:
+        MockSession.return_value.get_credentials.return_value.access_key = 'fake_access_key'
+        MockSession.return_value.get_credentials.return_value.secret_key = 'fake_secret_key'
+        MockSession.return_value.get_credentials.return_value.token = None
+        yield MockSession
+
+
 @patch('boto3.resource')
-def test_lambda_handler_success(mock_boto3_resource, mock_environment):
+def test_lambda_handler_success(mock_boto3_resource, mock_environment, mock_boto3_session):
     # Mock DynamoDB Table and its get_item method
     mock_table = MagicMock()
     mock_table.get_item.return_value = {
@@ -37,7 +46,7 @@ def test_lambda_handler_success(mock_boto3_resource, mock_environment):
 
 
 @patch('boto3.resource')
-def test_lambda_handler_resume_not_found(mock_boto3_resource, mock_environment):
+def test_lambda_handler_resume_not_found(mock_boto3_resource, mock_environment, mock_boto3_session):
     # Mock DynamoDB Table and its get_item method to return no item
     mock_table = MagicMock()
     mock_table.get_item.return_value = {}
@@ -56,7 +65,7 @@ def test_lambda_handler_resume_not_found(mock_boto3_resource, mock_environment):
 
 
 @patch('boto3.resource')
-def test_lambda_handler_internal_server_error(mock_boto3_resource, mock_environment):
+def test_lambda_handler_internal_server_error(mock_boto3_resource, mock_environment, mock_boto3_session):
     # Mock DynamoDB Table to raise an exception
     mock_table = MagicMock()
     mock_table.get_item.side_effect = Exception("Test exception")
